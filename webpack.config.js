680b4a32a58e275a1039434db5b5b1e82961d34c
gz65555 <gz65555@126.com>
2017-01-18 01:10:06 +0800

    delete externals

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,5 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        'jquery': 'window.$',
-        'bootstrap':'bootstrap'
     },
 };
